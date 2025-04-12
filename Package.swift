77abe731b72b4660796907533fa4056a6587dcab
Nayan Bhut <nayan.bhut@brainvire.com>
2025-04-12 22:50:12 +0530

    Init

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "LocationTrack",
     dependencies: [
         // 💧 A server-side Swift web framework. 
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(
